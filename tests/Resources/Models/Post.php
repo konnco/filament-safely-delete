cb40aa1128b4a47ad7737833b55e88c59cbcedc5
frankyso <frankyso@users.noreply.github.com>
2022-09-27 12:34:40 +0000

    Fix styling

--- tests/Resources/Models/Post.php
+++ tests/Resources/Models/Post.php
@@ -3,7 +3,6 @@
 namespace Konnco\FilamentSafelyDelete\Tests\Resources\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
