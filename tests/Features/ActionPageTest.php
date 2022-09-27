4e20d69fb6137e5670ea2431ce187ba211452bb6
Franky So <frankyso.mail@gmail.com>
2022-09-27 16:53:53 +0700

    Added Testcase for Table type

---
+++ tests/Features/ActionPageTest.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Str;
+use Konnco\FilamentSafelyDelete\Pages\Actions\DeleteAction;
+use Konnco\FilamentSafelyDelete\Tests\Resources\Models\Post;
+use function Pest\Laravel\assertDatabaseCount;
+
+it('can delete with confirmation', function () {
+    $post = (new Post)->forceFill(['title' => fake()->title, 'slug' => fake()->slug, 'body' => fake()->text]);
+    $post->save();
+
+    livewire()
+        ->callTableAction(DeleteAction::class, $post, data: [
+            'name' => $post->title,
+        ])->assertHasNoTableActionErrors();
+
+    assertDatabaseCount(Post::class, 0);
+});
+
+it('can validate the confirmation name', function () {
+    $post = (new Post)->forceFill(['title' => fake()->title, 'slug' => fake()->slug, 'body' => fake()->text]);
+    $post->save();
+
+    livewire()
+        ->callTableAction(DeleteAction::class, $post, data: [
+            'name' => $post->title.Str::random(2),
+        ])->assertHasTableActionErrors();
+
+    assertDatabaseCount(Post::class, 1);
+});
+
+//it('can delete with bulk actions and should type member current password', function () {
+//});
+
+//it('can change the method into delete rollback functions', function () {
+//});
+
+//it('can change the method into delete rollback functions', function () {
+//});
